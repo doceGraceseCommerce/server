cd90c77f5a801347c274df0d3381fab666331f7f
Diego Silva <diegorodrigocpv@outlook.com>
2021-10-09 20:21:22 -0300

    Adicionando mecanismo de CORS

--- src/main/java/docesgraces/server/controller/LoginController.java
+++ src/main/java/docesgraces/server/controller/LoginController.java
@@ -3,6 +3,7 @@ package docesgraces.server.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import docesgraces.server.model.Usuario;
 import docesgraces.server.service.AuthenticationService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/login")
 public class LoginController {
 
--- src/main/java/docesgraces/server/controller/UsuarioController.java
+++ src/main/java/docesgraces/server/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -20,6 +21,7 @@ import docesgraces.server.repository.UsuarioRepository;
 import docesgraces.server.service.AuthenticationService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
@@ -29,10 +31,10 @@ public class UsuarioController {
 	@Autowired
 	private AuthenticationService authenticationService;
 
-//	@GetMapping
-//	public List<Usuario> listar() {
-//		return usuarioRepository.findAll();
-//	}
+	@GetMapping
+	public List<Usuario> listar() {
+		return usuarioRepository.findAll();
+	}
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
